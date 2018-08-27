c662edea4962370e468daa4485d9e4f8c0b457a2
James Heggs <james.heggs@closconsultancy.com>
2018-08-27 15:17:37 +0100

    A failing test

--- services/calculator.test.js
+++ services/calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require('./calculator');
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.sum(1, 2)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 7 - should fail', () => {
+  expect(calculator.sum(1, 2)).toBe(7);
+});
